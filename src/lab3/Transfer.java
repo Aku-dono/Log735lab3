06585c4314afd0a5395ebf334d1485f8ba84bdb7
Noneofyour Business <Gotan25@hotmail.com>
2017-06-18 21:53:41 -0400

    implemented Chandy-Lamport manual request, still somewhat buggy. Need to combine floating messages.

---
+++ src/lab3/Transfer.java
@@ -0,0 +1,34 @@
+package lab3;
+
+import java.io.Serializable;
+
+public class Transfer implements Serializable {
+	private int money;
+	private int senderID;
+	private int receiverID; 
+	private int snapshotToken;
+	
+	public int getMoney() {
+		return money;
+	}
+
+	public int getSenderID() {
+		return senderID;
+	}
+
+	public int getReceiverID() {
+		return receiverID;
+	}
+
+	public int getSnapshotToken() {
+		return snapshotToken;
+	}
+
+	public Transfer(int money, int senderID, int receiverID, int snapshotToken)
+	{
+		this.money = money;
+		this.senderID = senderID;
+		this.receiverID = receiverID;
+		this.snapshotToken = snapshotToken;
+	}
+}
